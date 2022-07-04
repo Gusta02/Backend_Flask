1c7e7cbf9f06db2cd0ba6867cdf40d2d81792020
Michel Florencio <84158543+michblez@users.noreply.github.com>
2022-07-04 14:38:27 -0300

    Fix/ConsultaTransporte/CardsFront
    Reparo na consulta do transporte, reparo metodo dinamico.
    
    Funcoes adicionadas na trasnporte.py
    
    Adicionado Cards no front, Pagina Transporte e Home

--- app/Dash_Logistica/transporte.py
+++ app/Dash_Logistica/transporte.py
@@ -26,8 +26,34 @@ def frete_arrecadado():
 
         return resultado
 
+#//////////////////// MICHEL //////////////////////////
+
+def TotalAvaria(): #///////////////  CARD TOTAL AVARIAS =  Card 6 //////////////////////// 
+
+    Avaria = Transporte.Quant_avaria()
+    TotalAvaria = Avaria[0]['QuantidadeAvaria']
+
+    return TotalAvaria
+
+
+def Perc_TaxaAvaria(): #////////////////    CARD PERCENTUAL AVARIAS / ENTREGUE (TAXA)  = Card 7 ////////////////
+
+    Avaria = Transporte.Quant_avaria()
+    TotalAvaria = Avaria[0]['QuantidadeAvaria']
+
+    Entregue = Transporte.Quant_entregue()
+    TotalEntregue = Entregue[0]['Quantidade_Entregue']
+    TaxaAvaria = TotalAvaria / TotalEntregue 
+
+    # Percentual taxa Avaria
+    TaxaAvaria = "{:.2%}".format(TaxaAvaria)
+
+    return TaxaAvaria
+
 @transporte.route("/dashboard/logistica/transporte", methods=["GET","POST"])
 def Transporte_rota():
 
     frete_total = frete_arrecadado()
-    return render_template('Relatorio_transporte.html', frete = frete_total, card1=f'{kpi_entregues_no_prazo: .0%}',card2 = kpi_pedidos_ja_atrasados, card3 = f'{pct_entregas_sem_etapa_7: .0%}', card4 = f'{pct_entregas_sem_etapa_19: .0%}', card5 = '', card6 = '', card7 = '')
+    card6 = TotalAvaria() 
+    card7 = Perc_TaxaAvaria()
+    return render_template('Relatorio_transporte.html', frete = frete_total, card1=f'{kpi_entregues_no_prazo: .0%}',card2 = kpi_pedidos_ja_atrasados, card3 = f'{pct_entregas_sem_etapa_7: .0%}', card4 = f'{pct_entregas_sem_etapa_19: .0%}', card5 = '', card6 = card6, card7 = card7)
