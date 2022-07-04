76a02b9d53effb3f397168a94f5806797dd2d2ef
Gusta03 <90052920+Gusta03@users.noreply.github.com>
2022-07-04 11:30:25 -0300

    fix/atualizacao_nomes_frete_arrecadado

--- app/Dash_Logistica/transporte.py
+++ app/Dash_Logistica/transporte.py
@@ -1,14 +1,32 @@
 from flask import Blueprint, render_template, request
 from sqlalchemy import true
-from ..controllers.controller_logistica import IntegracaoWms
+from ..controllers.controller_logistica import Transporte
 import pandas as pd
 from datetime import datetime
+import locale
 from ..Dash_Logistica.kpis_luiz.main import kpi_entregues_no_prazo,kpi_pedidos_ja_atrasados,pct_entregas_sem_etapa_19,pct_entregas_sem_etapa_7
 
 transporte = Blueprint('transporte', __name__ , template_folder='templates', static_folder='static',  static_url_path='/app/Dash_Logistica/static/')
 
 
-@transporte.route("/dashboard/logistica/transporte", methods=["GET","POST"])
-def Transporte():
+def frete_arrecadado():
 
-    return render_template('Relatorio_transporte.html',card1=f'{kpi_entregues_no_prazo: .0%}',card2 = kpi_pedidos_ja_atrasados, card3 = f'{pct_entregas_sem_etapa_7: .0%}', card4 = f'{pct_entregas_sem_etapa_19: .0%}', card5 = '', card6 = '', card7 = '')
+    pedido = Transporte.Valor_arrecadado_frete_pedido()
+    showroom = Transporte.Valor_arrecadado_frete_showrrom()
+
+    pedido = pedido[0]['FreteRecebido']
+    showroom = showroom[0]['FreteRecebido']
+
+    resultado = (int(pedido) + int(showroom))
+
+    resultado = locale.currency(resultado)
+    return resultado
+
+
+@transporte.route("/dashboard/logistica/transporte", methods=["GET","POST"])
+def Transporte_rota():
+
+    frete_total = frete_arrecadado()
+    print(frete_total)
+
+    return render_template('Relatorio_transporte.html',frete = frete_total, card1=f'{kpi_entregues_no_prazo: .0%}',card2 = kpi_pedidos_ja_atrasados, card3 = f'{pct_entregas_sem_etapa_7: .0%}', card4 = f'{pct_entregas_sem_etapa_19: .0%}', card5 = '', card6 = '', card7 = '')
