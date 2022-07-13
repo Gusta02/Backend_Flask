c88d3b61c96e0846edaaa14e67df9d8c02b61da4
Michel Florencio <84158543+michblez@users.noreply.github.com>
2022-07-13 10:39:43 -0300

    Fix/ ConsultasdeContas

--- app/Dash_Financeiro/kpis_michel/pagar.py
+++ app/Dash_Financeiro/kpis_michel/pagar.py
@@ -1,31 +1,60 @@
-from time import strftime
 import pandas as pd
-from flask import Flask 
-from datetime import datetime, timedelta, date
-from openpyxl import Workbook
+from datetime import timedelta, date
+from dateutil.relativedelta import *
 
 hoje = date.today() 
 Atual = hoje.strftime('%Y-%m-%d')
 
+#filtro 15 dias 
+quinze = timedelta(days=15)
+quinze_dias =  hoje + quinze
+quinze_dias = quinze_dias.strftime('%Y-%m-%d')
+
+#filtro 30 dias 
 trinta = timedelta(days=30)
 trinta_dias =hoje + trinta
 trinta_dias= trinta_dias.strftime('%Y-%m-%d')
 
-#filtro sessenta dias
+#filtro 60 dias
 sessenta = timedelta(days=60)
 sessenta_dias =  hoje + sessenta
 sessenta_dias = sessenta_dias.strftime('%Y-%m-%d')
 
-#filtro noventa dias
+#filtro 90 dias
 noventa = timedelta(days=90)
 noventa_dias =  hoje+ noventa
 noventa_dias = noventa_dias.strftime('%Y-%m-%d')
 
+#filtro 12 meses
+doze =  (hoje) + relativedelta(months= 12)
+doze_meses =  doze.strftime('%Y-%m-%d')
 
-#<<<<<<<<<<<<<    A PAGAR CONTAS   >>>>>>>>>>>>>>>>>>>>
 
+# filtro os dados do dia atual até os proximos 15 dias
+def Pagar_15dias():
+    data = pd.read_excel('pivot_202207.xlsx')
+    
+    data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
+    'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
+
+    data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
+    data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
+
+    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= quinze_dias)
+
+    data = data[f1]
+
+    data['TIPO'] == 'CONTAS A PAGAR'
+    
+    Filtro_pagar = data.loc[data['TIPO'] == 'CONTAS A PAGAR',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
+    'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
+
+    return Filtro_pagar
+
+
+# filtro os dados do dia atual até os proximos 30 dias
 def Pagar_30dias():
-    data = pd.read_excel('app/Dash_Financeiro/planilhas/pivot_202207.xlsx')
+    data = pd.read_excel('pivot_202207.xlsx')
     
     data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
     'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
@@ -42,13 +71,12 @@ def Pagar_30dias():
     Filtro_pagar = data.loc[data['TIPO'] == 'CONTAS A PAGAR',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
     'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
 
-    Filtro_pagar['DATA_VENCIMENTO'] = strftime('%d-%m-%Y')
-
-    
     return Filtro_pagar
 
+
+# filtro os dados do dia atual até os proximos 60 dias
 def Pagar_60dias():
-    data = pd.read_excel('app/Dash_Financeiro/planilhas/pivot_202207.xlsx')
+    data = pd.read_excel('pivot_202207.xlsx')
     
     data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
     'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
@@ -64,13 +92,13 @@ def Pagar_60dias():
     
     Filtro_pagar = data.loc[data['TIPO'] == 'CONTAS A PAGAR',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
     'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
-
-    Filtro_pagar['DATA_VENCIMENTO'] = strftime('%d-%m-%Y')
     
     return Filtro_pagar
 
+
+# filtro os dados do dia atual até os proximos 90 dias
 def Pagar_90dias():
-    data = pd.read_excel('app/Dash_Financeiro/planilhas/pivot_202207.xlsx')
+    data = pd.read_excel('pivot_202207.xlsx')
     
     data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
     'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
@@ -86,15 +114,13 @@ def Pagar_90dias():
     
     Filtro_pagar = data.loc[data['TIPO'] == 'CONTAS A PAGAR',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
     'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
-    
-    Filtro_pagar['DATA_VENCIMENTO'] = strftime('%d-%m-%Y')
-    
+
     return Filtro_pagar
 
-#<<<<<<<<<<<<<    A RECEBER CONTAS >>>>>>>>>>>>>>>>>>>>
 
-def Receber_30dias():
-    data = pd.read_excel('app/Dash_Financeiro/planilhas/pivot_202207.xlsx')
+# filtro os dados do dia atual até os proximos 12 meses
+def Pagar_12meses():
+    data = pd.read_excel('pivot_202207.xlsx')
     
     data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
     'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
@@ -102,59 +128,13 @@ def Receber_30dias():
     data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
     data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
 
-    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= trinta_dias)
+    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= doze_meses)
 
     data = data[f1]
+
+    data['TIPO'] == 'CONTAS A PAGAR'
     
-    data['TIPO'] == 'CONTAS A RECEBER'
-    
-    Filtro_receber = data.loc[data['TIPO'] == 'CONTAS A RECEBER',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
+    Filtro_pagar = data.loc[data['TIPO'] == 'CONTAS A PAGAR',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
     'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
 
-    Filtro_receber['DATA_VENCIMENTO'] = strftime('%d-%m-%Y')
- 
-    return  Filtro_receber
-
-def Receber_60dias():
-    data = pd.read_excel('app/Dash_Financeiro/planilhas/pivot_202207.xlsx')
-    
-    data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
-    'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
-
-    data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
-    data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
-
-    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= sessenta_dias)
-
-    data = data[f1]
-    
-    data['TIPO'] == 'CONTAS A RECEBER'
-    
-    Filtro_receber = data.loc[data['TIPO'] == 'CONTAS A RECEBER',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
-    'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
-
-    Filtro_receber['DATA_VENCIMENTO'] = strftime('%d-%m-%Y')
-
-    return  Filtro_receber
-
-def Receber_90dias():
-    data = pd.read_excel('app/Dash_Financeiro/planilhas/pivot_202207.xlsx')
-    
-    data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
-    'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
-
-    data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
-    data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
-
-    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= noventa_dias)
-
-    data = data[f1]
-    
-    data['TIPO'] == 'CONTAS A RECEBER'
-    
-    Filtro_receber = data.loc[data['TIPO'] == 'CONTAS A RECEBER',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
-    'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
-
-    Filtro_receber['DATA_VENCIMENTO'] = strftime('%d-%m-%Y')
-
-    return Filtro_receber
+    return Filtro_pagar
---
+++ app/Dash_Financeiro/kpis_michel/receber.py
@@ -0,0 +1,138 @@
+import pandas as pd
+from datetime import timedelta, date
+from dateutil.relativedelta import *
+
+hoje = date.today() 
+Atual = hoje.strftime('%Y-%m-%d')
+
+#filtro 15 dias 
+quinze = timedelta(days=15)
+quinze_dias =  hoje + quinze
+quinze_dias = quinze_dias.strftime('%Y-%m-%d')
+
+#filtro 30 dias 
+trinta = timedelta(days=30)
+trinta_dias =hoje + trinta
+trinta_dias= trinta_dias.strftime('%Y-%m-%d')
+
+#filtro 60 dias
+sessenta = timedelta(days=60)
+sessenta_dias =  hoje + sessenta
+sessenta_dias = sessenta_dias.strftime('%Y-%m-%d')
+
+#filtro 90 dias
+noventa = timedelta(days=90)
+noventa_dias =  hoje+ noventa
+noventa_dias = noventa_dias.strftime('%Y-%m-%d')
+
+#filtro 12 meses
+doze =  (hoje) + relativedelta(months= 12)
+doze_meses =  doze.strftime('%Y-%m-%d')
+
+#tabela excel
+data = pd.read_excel('pivot_202207.xlsx')
+
+# filtro os dados do dia atual até os proximos 15 dias
+def Pagar_15dias():
+    data = pd.read_excel('pivot_202207.xlsx')
+    
+    data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
+    'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
+
+    data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
+    data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
+
+    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= quinze_dias)
+
+    data = data[f1]
+
+    data['TIPO'] == 'CONTAS A RECEBER'
+    
+    Filtro_pagar = data.loc[data['TIPO'] == 'CONTAS A RECEBER',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
+    'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
+
+    return Filtro_pagar
+
+# filtro os dados do dia atual até os proximos 30 dias
+def Receber_30dias():
+    data = pd.read_excel('pivot_202207.xlsx')
+    
+    data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
+    'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
+
+    data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
+    data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
+
+    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= trinta_dias)
+
+    data = data[f1]
+    
+    data['TIPO'] == 'CONTAS A RECEBER'
+    
+    Filtro_receber = data.loc[data['TIPO'] == 'CONTAS A RECEBER',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
+    'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
+ 
+    return  Filtro_receber
+
+# filtro os dados do dia atual até os proximos 60 dias
+def Receber_60dias():
+    data = pd.read_excel('pivot_202207.xlsx')
+    
+    data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
+    'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
+
+    data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
+    data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
+
+    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= sessenta_dias)
+
+    data = data[f1]
+    
+    data['TIPO'] == 'CONTAS A RECEBER'
+    
+    Filtro_receber = data.loc[data['TIPO'] == 'CONTAS A RECEBER',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
+    'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
+
+    return  Filtro_receber
+
+# filtro os dados do dia atual até os proximos 90dias
+def Receber_90dias():
+    data = pd.read_excel('pivot_202207.xlsx')
+    
+    data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
+    'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
+
+    data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
+    data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
+
+    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= noventa_dias)
+
+    data = data[f1]
+    
+    data['TIPO'] == 'CONTAS A RECEBER'
+    
+    Filtro_receber = data.loc[data['TIPO'] == 'CONTAS A RECEBER',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
+    'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
+
+    return Filtro_receber
+
+# filtro os dados do dia atual até os proximos 12 meses
+def Pagar_12meses():
+    data = pd.read_excel('pivot_202207.xlsx')
+    
+    data.columns =  ['DATA_EMITIDA','DATA_VENCIMENTO','TIPO','ORIGEM','SITUACAO','GRUPO','CATEGORIA',
+    'CLIENTE_FORNECEDOR','CNPJ_CPF','OBSERVACAO_CONTA','DOCUMENTO_TIPO','VALOR_CONTA','PAGO_RECEBIDO','A_RECEBER_PAGAR']
+
+    data.loc[:,'TIPO'] = data.loc[:,'TIPO'].apply(lambda x: str(x).split('.')[-1].strip().upper())
+    data['DATA_VENCIMENTO'] = data['DATA_VENCIMENTO'].apply(lambda k: str(k).split()[0])
+
+    f1 = (data['DATA_VENCIMENTO'] >= Atual) & (data['DATA_VENCIMENTO'] <= doze_meses)
+
+    data = data[f1]
+
+    data['TIPO'] == 'CONTAS A RECEBER'
+    
+    Filtro_pagar = data.loc[data['TIPO'] == 'CONTAS A RECEBER',['DATA_VENCIMENTO','TIPO','CATEGORIA','SITUACAO',
+    'CLIENTE_FORNECEDOR','OBSERVACAO_CONTA','VALOR_CONTA']]
+
+    return Filtro_pagar
