d9dc42bf3333b8bb168e353322c0240be13fe122
Gusta03 <90052920+Gusta03@users.noreply.github.com>
2022-07-13 11:29:33 -0300

    fix/telas_contas_a_receber_e_pagar

--- app/Dash_Financeiro/ContasaPagar.py
+++ app/Dash_Financeiro/ContasaPagar.py
@@ -8,3 +8,7 @@ import io
 import pandas as pd
 
 Contas_Pagar = Blueprint('Contas_Pagar', __name__ , template_folder='templates', static_folder='static',  static_url_path='/app/Dash_Logistica/static/')
+
+@Contas_Pagar.route("/dashboard/financeiro/ContasPagar", methods=["GET","POST"])
+def Contas_a_Pagar():
+    return render_template('contas_a_pagar.html', page = 1)
